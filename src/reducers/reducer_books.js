5e24035b0cddc67aba389b3bea6d0d84a83bf21a
Israel Alvarez <ialvarez@nyc01alee.local>
2018-05-09 15:44:55 -0400

    add list of books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// simple function that returns an array of books
+export default function() {
+    return [
+        { title: 'Harry Potter 1'},
+        { title: 'Harry Potter 2'},
+        { title: 'Harry Potter 3'},
+        { title: 'Harry Potter 4'},
+    ]
+}
