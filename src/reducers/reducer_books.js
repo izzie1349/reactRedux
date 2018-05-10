20a993815e927eeaefd03e8e1f7d3a4e76af9229
Israel Alvarez <ialvarez@nyc01alee.local>
2018-05-10 17:02:07 -0400

    modify container to show changes in reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 // simple function that returns an array of books
 export default function() {
     return [
-        { title: 'Harry Potter 1'},
-        { title: 'Harry Potter 2'},
-        { title: 'Harry Potter 3'},
-        { title: 'Harry Potter 4'},
+        { title: 'Harry Potter 1', pages: 101},
+        { title: 'Harry Potter 2', pages: 345},
+        { title: 'Harry Potter 3', pages: 798},
+        { title: 'Harry Potter 4', pages: 345},
     ]
 }
