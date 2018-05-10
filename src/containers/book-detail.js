48b2b5facde0b25d94f7e14ac02fff3e83d6d353
Israel Alvarez <ialvarez@nyc01alee.local>
2018-05-10 16:24:58 -0400

    connect container to redux store

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
     render() {
         return(
             <div>Book Detail!</div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    // object returned is props in BookDetail
+    return {
+        book: state.activeBook
+    };
+}
+
+export default connect(mapStateToProps)(BookDetail);
