70098a7461a8409a556e70ba6bea1d2aa039b3c7
Israel Alvarez <ialvarez@nyc01alee.local>
2018-05-10 15:55:56 -0400

    modify action to be more realistic

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
 export function selectBook(book) {
-    console.log('a book has been selected: ', book.title);
+    // console.log('a book has been selected: ', book.title);
+
+    // selectBook is a ActionCreator, it needs to return an action,
+    // an object with a type proprty
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    }
 }
