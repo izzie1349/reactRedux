2104594c64fe38ed2803b929ddf952e86e8b482a
Israel Alvarez <ialvarez@nyc01alee.local>
2018-05-10 16:10:10 -0400

    import activeBook reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+// State arg is not app state, only the State
+// this reducer is responsible for
+export default function(state = null, action) { //state cant be undefined null
+    switch(action.type){
+        case 'BOOK_SELECTED':
+            return action.payload
+    }
+    return state
+}
